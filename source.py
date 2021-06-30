e659c17406e246153421955fe19a748f80590c15
Tom Albone <tbalbone@gmail.com>
2021-06-30 10:28:25 +0100

    2nd Draft of notebook underway.

--- source.py
+++ source.py
@@ -272,23 +272,26 @@ def invalid_op(data):
     #return dataf
 
 
-def mean_hourly(dataf, freq, *year):
-
-    if year:
-        dataf = dataf.loc[dataf.BRCYear == year]
+def mean_hourly(dataf, freq):
 
     if freq == "day":
         dataf = dataf.groupby([
-            pd.Grouper(key="DateTime",freq="D"),'BRCWeekNum','BRCMonth'])['Count'].aggregate(np.mean)
+            pd.Grouper(key="DateTime",freq="D"),'BRCWeekNum','BRCMonth','BRCYear'])['Count'].aggregate(np.mean)
     elif freq == "month":
         dataf = dataf.groupby(
             ['BRCMonthNum',pd.Grouper(key="BRCMonth"),'BRCYear'])['Count'].aggregate(np.mean).reset_index()
     elif freq == "week":
         dataf = dataf.set_index('DateTime').groupby(
-            [pd.Grouper(key="BRCWeekNum")])['Count'].aggregate(np.mean)
+            [pd.Grouper(key="BRCWeekNum"),'BRCYear'])['Count'].aggregate(np.mean)
     elif freq == "year":
         dataf = dataf.set_index('DateTime').groupby(
             [pd.Grouper(key="BRCYear")])['Count'].aggregate(np.mean)
+
+    return dataf
+
+def remove_new_cameras(dataf):
+
+    dataf.drop(dataf[ (dataf['Location'] == "Albion Street at McDonalds") & (dataf['Location'] == "Park Row")].index,inplace=True)
 
     return dataf
 
@@ -331,8 +334,49 @@ def create_sum_df(data, time, year):
 
     return resample_function(data)
 
-def chart_lockdown_dates(fig,vline_anno,vrec_anno):
+def chart_lockdown_dates(fig):
+    # Create a dictionary of annotation parameters for the Plotly vertical lines
+    vline_anno = {"date": ['2020-03-16',
+                           '2020-03-23',
+                           '2020-05-10',
+                           '2020-06-01',
+                           '2020-06-15',
+                           '2020-10-14',
+                           '2020-11-05',
+                           '2020-09-22',
+                           '2020-12-02',
+                           '2020-08-03',
+                           '2021-01-05',
+                           '2021-03-08',
+                           '2021-03-29',
+                           '2021-04-12'],
 
+                  "text": ["16th Mar",
+                           "23rd Mar",
+                           "10th <br> May",
+                           "1st Jun",
+                           "15th <br> Jun",
+                           '14th Oct',
+                           '5th <br> Nov',
+                           '22nd <br> Sept',
+                           '2nd <br> Dec',
+                           '3rd <br> Aug',
+                           '2nd <br> Jan',
+                           '8th <br> Mar',
+                           '29th Mar',
+                           '12th <br> Apr'],
+
+                  "textangle": [0, -90, 0, -90, 0, -90, 0, 0, 0, 0, 0, 0, -90, 0],
+                  "position": ["top", "right", "top", "left", "top", "left", "top", "top", "top", "top", "top", "top",
+                               "left", "top"],
+                  "showarrow": [True, False, False, False, False, False, False, False, False, False, False, False,
+                                False, False]
+                  }
+    # Create a dictionary of annotation parameters for the Plotly vertical rectangles
+    vrec_anno = {"x0": ['2020-03-23', '2020-06-15', '2020-11-05', '2020-12-02', '2021-01-05', '2021-03-29'],
+                 "x1": ['2020-06-15', '2020-11-05', '2020-12-02', '2021-01-05', '2021-03-29', '2021-04-25'],
+                 "fillcolor": ['red', 'orange', 'red', 'orange', 'red', 'orange']
+                 }
 
     for i, date in enumerate(vline_anno['date']):
         fig.add_vline(
@@ -373,7 +417,375 @@ def combine_cameras(dataf):
     return dataf
 
 
-def set_start_date(dataf):
-    dataf = dataf.loc[dataf.DateTime >= '2008-08-27']
+def set_start_date(dataf,date):
+    dataf = dataf.loc[dataf.DateTime >= date]
+
+    return dataf
+
+
+
+def hosp_indoor(dataf):
+
+    dataf['hosp_indoor'] = 3
+    dataf.loc[(dataf.DateTime >= '2020-03-20') & (dataf.DateTime < '2020-06-15'),'hosp_indoor'] = 1
+    dataf.loc[(dataf.DateTime >= '2020-10-14') & (dataf.DateTime < '2020-11-02'), 'hosp_indoor'] = 2
+    dataf.loc[(dataf.DateTime >= '2020-11-02') & (dataf.DateTime < '2021-05-17'),'hosp_indoor'] = 1
+
+
+    return dataf
+
+def hosp_outdoor(dataf):
+
+    dataf['hosp_outdoor'] = 3
+    dataf.loc[(dataf.DateTime >= '2020-03-20') & (dataf.DateTime < '2020-06-15'),'hosp_outdoor'] = 1
+    dataf.loc[(dataf.DateTime >= '2020-10-14') & (dataf.DateTime < '2020-11-02'),'hosp_outdoor'] = 2
+    dataf.loc[(dataf.DateTime >= '2020-11-02') & (dataf.DateTime < '2021-04-12'),'hosp_outdoor'] = 1
+
+
+    return dataf
+
+def hotels(dataf):
+
+    dataf['hotels'] = 4
+    dataf.loc[(dataf.DateTime >= '2020-03-26') & (dataf.DateTime < '2020-07-04'),'hotels'] = 1
+    dataf.loc[(dataf.DateTime >= '2020-07-04') & (dataf.DateTime < '2020-11-02'), 'hotels'] = 3
+    dataf.loc[(dataf.DateTime >= '2020-11-05') & (dataf.DateTime < '2020-12-02'),'hotels'] = 1
+    dataf.loc[(dataf.DateTime >= '2020-11-02') & (dataf.DateTime < '2020-11-05'), 'hotels'] = 2
+    dataf.loc[(dataf.DateTime >= '2020-12-02') & (dataf.DateTime < '2021-01-06'),'hotels'] = 2
+    dataf.loc[(dataf.DateTime >= '2021-01-06') & (dataf.DateTime < '2021-05-17'), 'hotels'] = 1
+
+    return dataf
+
+def ent_indoor(dataf):
+
+    #default value of open with no restrictions
+    dataf['ent_indoor'] = 5
+
+    #full closure until reopening on 4th July 2020
+    dataf.loc[(dataf.DateTime >= '2020-03-20') & (dataf.DateTime < '2020-07-04'), 'ent_indoor'] = 1
+
+    #reopens on 4th July with up to 30 people legally allowed until rule of 6 legally introduced on 14th Sept 2020
+    dataf.loc[(dataf.DateTime >= '2020-07-04') & (dataf.DateTime < '2020-09-14'), 'ent_indoor'] = 4
+
+    #open with rule of 6 until 14th October 2020 when put into tier 2
+    dataf.loc[(dataf.DateTime >= '2020-09-14') & (dataf.DateTime < '2020-10-14'),'ent_indoor'] = 3
+
+    #open but only with household until 2nd November when put into tier 3
+    dataf.loc[(dataf.DateTime >= '2020-10-14') & (dataf.DateTime < '2021-11-02'), 'ent_indoor'] = 2
+
+    #full closed after being put in tier 3, through national lockdown 2, Christmas in tier 3 and national lockdown 3.
+    dataf.loc[(dataf.DateTime >= '2020-11-02') & (dataf.DateTime < '2021-05-17'), 'ent_indoor'] = 1
+
+    #Reopens to rule of 6 on 17th May 2020
+    dataf.loc[(dataf.DateTime >= '2021-05-17'),'ent_indoor'] = 3
+
+    return dataf
+
+def ent_outdoor(dataf):
+
+    # default value of open with no restrictions
+    dataf['ent_outdoor'] = 5
+
+    # full closure until reopening on 4th July 2020
+    dataf.loc[(dataf.DateTime >= '2020-03-20') & (dataf.DateTime < '2020-07-04'), 'ent_outdoor'] = 1
+
+    # reopens on 4th July with up to 30 people legally allowed until rule of 6 legally introduced on 14th Sept 2020
+    dataf.loc[(dataf.DateTime >= '2020-07-04') & (dataf.DateTime < '2020-09-14'), 'ent_outdoor'] = 5
+
+    # open with rule of 6 until 5th november 2020 when national lockdown starts
+    dataf.loc[(dataf.DateTime >= '2020-09-14') & (dataf.DateTime < '2020-11-05'), 'ent_outdoor'] = 3
+
+    # full closure during 2nd national lockdown until put back into tier 3 on 2nd December 2020.
+    dataf.loc[(dataf.DateTime >= '2020-11-05') & (dataf.DateTime < '2020-12-02'), 'ent_outdoor'] = 1
+
+    # open with rule of 6 until 6th January 2021 when 3rd national lockdown starts
+    dataf.loc[(dataf.DateTime >= '2020-12-02') & (dataf.DateTime < '2021-01-06'), 'ent_outdoor'] = 3
+
+    # full closure during 3rd national lockdown until reopens on 12th April 2021
+    dataf.loc[(dataf.DateTime >= '2021-01-06') & (dataf.DateTime < '2021-04-12'), 'ent_outdoor'] = 1
+
+    # Reopens to rule of 6 on 12th April 2021
+    dataf.loc[(dataf.DateTime >= '2021-04-12'), 'ent_outdoor'] = 3
+
+    return dataf
+
+def weddings(dataf):
+
+    # default value of Yes with no restrictions
+    dataf['weddings'] = 5
+
+    #Fully banned during lockdown 1 until restrictions eased on 4th July
+    dataf.loc[(dataf.DateTime >= '2020-03-23') & (dataf.DateTime < '2020-07-04'), 'weddings'] = 1
+
+    #Weddings of up to 30 people allowed
+    dataf.loc[(dataf.DateTime >= '2020-07-04') & (dataf.DateTime < '2020-09-28'), 'weddings'] = 4
+
+    #Weddings of up to 15 people allowed
+    dataf.loc[(dataf.DateTime >= '2020-09-28') & (dataf.DateTime < '2020-11-05'), 'weddings'] = 3
+
+    #Weddings banned during lockdown 2 until restrictions eased on 2nd December
+    dataf.loc[(dataf.DateTime >= '2020-11-05') & (dataf.DateTime < '2020-12-02'), 'weddings'] = 1
+
+    # Weddings of up to 15 people allowed until start of lockdown 3 in January 2021
+    dataf.loc[(dataf.DateTime >= '2020-12-02') & (dataf.DateTime < '2021-01-06'), 'weddings'] = 3
+
+    # Weddings banned during lockdown 3 until restrictions eased on 29th March 2021
+    dataf.loc[(dataf.DateTime >= '2021-01-05') & (dataf.DateTime < '2021-03-29'), 'weddings'] = 1
+
+    # Weddings of up to 6 people allowed until 12th April 2021
+    dataf.loc[(dataf.DateTime >= '2021-03-29') & (dataf.DateTime < '2021-04-12'), 'weddings'] = 2
+
+    # Weddings of up to 15 people allowed until 17th May 2021
+    dataf.loc[(dataf.DateTime >= '2021-04-12') & (dataf.DateTime < '2021-05-17'), 'weddings'] = 3
+
+    # Weddings of up to 30 people allowed until 21st June 2021
+    dataf.loc[(dataf.DateTime >= '2021-05-17') & (dataf.DateTime < '2021-06-21'), 'weddings'] = 4
+
+    return dataf
+
+def self_acc(dataf):
+
+    dataf['self_acc'] = 5
+
+    #Fully banned during lockdown 1 until restrictions eased on 4th July
+    dataf.loc[(dataf.DateTime >= '2020-03-23') & (dataf.DateTime < '2020-07-04'), 'self_acc'] = 1
+
+    #Allowed with max legal limits of 30 people up to rule of 6 on 14th September
+    dataf.loc[(dataf.DateTime >= '2020-07-04') & (dataf.DateTime < '2020-09-14'), 'self_acc'] = 4
+
+    #Rule of 6
+    dataf.loc[(dataf.DateTime >= '2020-09-14') & (dataf.DateTime < '2020-10-14'), 'self_acc'] = 3
+
+    #Household only
+    dataf.loc[(dataf.DateTime >= '2020-10-14') & (dataf.DateTime < '2020-11-05'), 'self_acc'] = 2
+
+    #Fully banned during lockdown 2 until special Christmas rules 24-26th December
+    dataf.loc[(dataf.DateTime >= '2020-11-05') & (dataf.DateTime < '2020-12-24'), 'self_acc'] = 1
+
+    #Special christmas rules allow more than one household of any size up to 3 households to get together.  Just classify as rule of 6 for the purposes of modelling
+    dataf.loc[(dataf.DateTime >= '2020-12-24') & (dataf.DateTime < '2020-12-27'), 'self_acc'] = 3
+
+    #Fully banned under tier 3 and all through national lockdown 3 until 12th April 2021
+    dataf.loc[(dataf.DateTime >= '2020-12-27') & (dataf.DateTime < '2021-04-12'), 'self_acc'] = 1
+
+    #Household only
+    dataf.loc[(dataf.DateTime >= '2021-04-12') & (dataf.DateTime < '2021-05-17'), 'self_acc'] = 2
+
+    #Household only
+    dataf.loc[dataf.DateTime >= '2021-05-17', 'self_acc'] = 2
+
+    return dataf
+
+def sport_lei_indoor(dataf):
+
+    #Default values of open with no restrictions
+    dataf['sport_lei_indoor'] = 5
+
+    #Fully banned during lockdown 1 until restrictions eased on 25th July
+    dataf.loc[(dataf.DateTime >= '2020-03-23') & (dataf.DateTime < '2020-07-25'), 'sport_lei_indoor'] = 1
+
+    #Reopen legally for groups of up to 30 (although guidance states rule of 6)
+    dataf.loc[(dataf.DateTime >= '2020-07-25') & (dataf.DateTime < '2020-09-14'), 'sport_lei_indoor'] = 4
+
+    #Open with rule of 6
+    dataf.loc[(dataf.DateTime >= '2020-09-14') & (dataf.DateTime < '2020-10-14'), 'sport_lei_indoor'] = 3
+
+    #Household only
+    dataf.loc[(dataf.DateTime >= '2020-10-14') & (dataf.DateTime < '2020-11-05'), 'sport_lei_indoor'] = 4
+
+    #Fully banned during lockdown 2, through tier 3 and lockdown 3 until restrictions eased on 12 April
+    dataf.loc[(dataf.DateTime >= '2020-11-05') & (dataf.DateTime < '2021-04-12'), 'sport_lei_indoor'] = 1
+
+    #Open to household only
+    dataf.loc[(dataf.DateTime >= '2021-04-12'), 'sport_lei_indoor'] = 1
+
+    return dataf
+
+def sport_lei_outdoor(dataf):
+
+    dataf['sport_lei_outdoor'] = 5
+
+    #Fully banned during lockdown 1 until restrictions eased on 4th July
+    dataf.loc[(dataf.DateTime >= '2020-03-23') & (dataf.DateTime < '2020-07-04'), 'sport_lei_outdoor'] = 1
+
+    #No restrictions on organised sport or leisure organised formally
+    dataf.loc[(dataf.DateTime >= '2020-07-04') & (dataf.DateTime < '2020-11-05'), 'sport_lei_outdoor'] = 5
+
+    #Fully banned during lockdown 2, through tier 3 until restrictions eased on
+    dataf.loc[(dataf.DateTime >= '2020-11-05') & (dataf.DateTime < '2021-03-29'), 'sport_lei_outdoor'] = 1
+
+    #Fully banned during lockdown 1 until restrictions eased on 4th July
+    dataf.loc[(dataf.DateTime >= '2021-03-29'), 'sport_lei_outdoor'] = 5
+
+    return dataf
+
+def non_essential_retail(dataf):
+
+    dataf['non_ess_retail'] = 1
+
+    #Fully closed during lockdown 1 until restrictions eased on 15th June
+    dataf.loc[(dataf.DateTime >= '2020-03-23') & (dataf.DateTime < '2020-06-15'), 'non_ess_retail'] = 0
+
+    #Fully closed during lockdown  until restrictions eased on 2nd December
+    dataf.loc[(dataf.DateTime >= '2020-11-05') & (dataf.DateTime < '2020-12-02'), 'non_ess_retail'] = 0
+
+    #Fully closed during lockdown 3 until restrictions eased on 12th April
+    dataf.loc[(dataf.DateTime >= '2021-01-05') & (dataf.DateTime < '2021-04-12'), 'non_ess_retail'] = 0
+
+    return dataf
+
+def primary_schools(dataf):
+
+    dataf['prim_sch'] = 1
+
+    #Fully closed during lockdown 1 until restrictions eased on 1st June 2020
+    dataf.loc[(dataf.DateTime >= '2020-03-23') & (dataf.DateTime < '2020-06-01'), 'prim_sch'] = 0
+
+    #Fully closed during lockdown 3 until restrictions eased on 8th March 2021
+    dataf.loc[(dataf.DateTime >= '2021-01-06') & (dataf.DateTime < '2021-03-08'), 'prim_sch'] = 0
+
+    return dataf
+
+def secondary_schools(dataf):
+
+    dataf['sec_sch'] = 1
+
+    #Fully closed during lockdown until restrictions eased on
+    dataf.loc[(dataf.DateTime >= '2020-03-23') & (dataf.DateTime < '2020-06-15'), 'sec_sch'] = 0
+
+    #Fully closed during lockdown until restrictions eased on
+    dataf.loc[(dataf.DateTime >= '2021-01-06') & (dataf.DateTime < '2021-03-08'), 'sec_sch'] = 0
+
+    return dataf
+
+def university(dataf):
+
+    #Open or blended learning
+    dataf['uni_campus'] = 1
+
+    #Mostly closed during lockdown until start of 2020/2021 academic year
+    dataf.loc[(dataf.DateTime >= '2020-03-23') & (dataf.DateTime < '2020-09-01'), 'uni_campus'] = 0
+
+    #Mostly closed during lockdown 3 until restrictions eased on 17th May
+    dataf.loc[(dataf.DateTime >= '2021-01-05'), 'uni_campus'] = 0
+
+    return dataf
+
+def outdoor_grp_public(dataf):
+
+    dataf['outdoor_grp_public'] = 5
+
+    #Max two people gathering outside of household
+    dataf.loc[(dataf.DateTime >= '2020-03-23') & (dataf.DateTime < '2020-06-01'), 'outdoor_grp_public'] = 2
+
+    #Max 6 people gathering
+    dataf.loc[(dataf.DateTime >= '2020-06-01') & (dataf.DateTime < '2020-07-04'), 'outdoor_grp_public'] = 3
+
+    #Max 30 people gathering (although rule of 6 as 'guidance')
+    dataf.loc[(dataf.DateTime >= '2020-07-04') & (dataf.DateTime < '2020-09-14'), 'outdoor_grp_public'] = 4
+
+    #Rule of 6 becomes legal
+    dataf.loc[(dataf.DateTime >= '2020-09-14') & (dataf.DateTime < '2020-11-05'), 'outdoor_grp_public'] = 3
+
+    #Max two people gathering outside of household
+    dataf.loc[(dataf.DateTime >= '2020-11-05') & (dataf.DateTime < '2020-12-02'), 'outdoor_grp_public'] = 2
+
+    #Rule of 6
+    dataf.loc[(dataf.DateTime >= '2020-12-02') & (dataf.DateTime < '2021-01-05'), 'outdoor_grp_public'] = 3
+
+    #Max two people gathering outside of household
+    dataf.loc[(dataf.DateTime >= '2021-01-05') & (dataf.DateTime < '2021-03-29'), 'outdoor_grp_public'] = 2
+
+    #Rule of 6
+    dataf.loc[(dataf.DateTime >= '2021-03-29'), 'outdoor_grp_public'] = 3
+
+    return dataf
+
+def outdoor_grp_private(dataf):
+
+    dataf['outdoor_grp_private'] = 5
+
+    # Max two people gathering outside of household
+    dataf.loc[(dataf.DateTime >= '2020-03-23') & (dataf.DateTime < '2020-06-01'), 'outdoor_grp_private'] = 2
+
+    #Max 6 people gathering
+    dataf.loc[(dataf.DateTime >= '2020-06-01') & (dataf.DateTime < '2020-07-04'), 'outdoor_grp_private'] = 3
+
+    #Max 30 people gathering (although rule of 6 as 'guidance')
+    dataf.loc[(dataf.DateTime >= '2020-07-04') & (dataf.DateTime < '2020-09-14'), 'outdoor_grp_private'] = 4
+
+    #Rule of 6 becomes legal
+    dataf.loc[(dataf.DateTime >= '2020-09-14') & (dataf.DateTime < '2020-11-02'), 'outdoor_grp_private'] = 3
+
+    #Household only
+    dataf.loc[(dataf.DateTime >= '2020-11-02') & (dataf.DateTime < '2020-11-05'), 'outdoor_grp_private'] = 1
+
+    #Max two people gathering outside of household
+    dataf.loc[(dataf.DateTime >= '2020-11-05') & (dataf.DateTime < '2020-12-02'), 'outdoor_grp_private'] = 2
+
+    #Household only
+    dataf.loc[(dataf.DateTime >= '2020-12-02') & (dataf.DateTime < '2021-03-29'), 'outdoor_grp_private'] = 1
+
+    #Rule of 6
+    dataf.loc[(dataf.DateTime >= '2021-03-29'), 'outdoor_grp_private'] = 3
+
+    return dataf
+
+def indoor_grp(dataf):
+
+    dataf['indoor_grp'] = 4
+
+    # Household group only
+    dataf.loc[(dataf.DateTime >= '2020-03-23') & (dataf.DateTime < '2020-07-04'), 'indoor_grp'] = 1
+
+    #Max 30 people gathering (although rule of 6 as 'guidance')
+    dataf.loc[(dataf.DateTime >= '2020-07-04') & (dataf.DateTime < '2020-09-14'), 'indoor_grp'] = 3
+
+    #Rule of 6
+    dataf.loc[(dataf.DateTime >= '2020-09-14') & (dataf.DateTime < '2020-10-14'), 'indoor_grp'] = 2
+
+    #Household only
+    dataf.loc[(dataf.DateTime >= '2020-10-14') & (dataf.DateTime < '2020-12-24'), 'indoor_grp'] = 1
+
+    #Special christmas rules allow more than one household of any size up to 3 households to get together.  Just classify as rule of 6 for the purposes of modelling
+    dataf.loc[(dataf.DateTime >= '2020-12-24') & (dataf.DateTime < '2020-12-27'), 'indoor_grp'] = 2
+
+    #Household only
+    dataf.loc[(dataf.DateTime >= '2020-12-27'), 'indoor_grp'] = 1
+
+    return dataf
+
+def eat_out(dataf):
+
+    dataf['eat_out'] = 0
+
+    #Eat out to Help out scheme active, encouraging people to go and use hospitality venues.
+    dataf.loc[(dataf.DateTime >= '2020-08-03') & (dataf.DateTime <= '2020-08-31'), 'eat_out'] = 1
+
+    return dataf
+
+
+def create_lockdown_var(dataf):
+
+    lockdown_var_list = [hosp_indoor,
+                         hosp_outdoor,
+                         hotels,
+                         ent_indoor,
+                         ent_outdoor,
+                         weddings,
+                         self_acc,
+                         sport_lei_indoor,
+                         sport_lei_outdoor,
+                         non_essential_retail,
+                         primary_schools,
+                         secondary_schools,
+                         university,
+                         outdoor_grp_public,
+                         outdoor_grp_private,
+                         indoor_grp,
+                         eat_out]
+
+    for func in lockdown_var_list:
+        dataf = func(dataf)
 
     return dataf
